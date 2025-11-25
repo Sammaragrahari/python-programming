37153da889b164cfc72c21d29642d9992513cd31
Sammaragrahari <agraharisammar@gmail.com>
2025-11-25 23:12:37 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,63 @@
+# print("start")
+# val =int(input("val:"))
+# if val == 1:
+#     print('iron man')
+# print("End")
+
+# val=int(input("val:"))
+# if val >10:
+#     print('sammar')
+
+# print("Start")
+# val = int(input("val:"))
+# if val==1:
+#     print('iron man')
+# else:
+#     print('superman')
+# print('end')
+
+
+# Write a program to check the number is even or odd
+# val=int(input("val:"))
+# if val%2==0:
+#     print("No is even")
+# else:
+#     print('No is odd')
+
+# Write a program to check enter the integer is between 5 and 50
+# val = int(input("val:"))
+# if val >5 and val < 50:
+#     print('number is belong to series')
+# else:
+#     print('number is not belong to series')
+
+# Write a program to check the student are pass or fail
+# print('Welcome to school')
+# marks=int(input("Enter the student marks:"))
+# if marks >=35:
+#     print("You are pass")
+# else:
+#     print("Best luck next time")
+
+# Write a program to check weather enter no is single digit or not
+# n= int(input("Enter the number:"))
+# if n >=0 and n <=9:
+#     print('Enter no is single digit')
+# else:
+#     print('Enter no is not single digit')
+
+# Write a program to check enter no is 2 digit or not
+# n =int(input("Enter no:"))
+# if n >= -99 and n <= -10:
+#     print("Enter no is 2 digit")
+# else:
+#     print("Enter no is not 2 digit")
+
+# Write a program to check enter character is vowel or not
+# n = (input(" Enter character :"))
+# vowel = ['A','E','I','O','U','a','e','i','o','u']
+# if n in vowel:
+#     print('Character is vowel')
+# else:
+#     print('Character is not vowel')
+
